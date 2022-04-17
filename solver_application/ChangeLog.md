1a331a4f3b5bd7d75ab61dc930721cd1a382255f
IvanGalk1n <ivangalkin0720@gmail.com>
2022-04-17 10:52:37 +0300

    testing tags

---
+++ solver_application/ChangeLog.md
@@ -0,0 +1,2 @@
+* 16.04.2022 IvanGalk1n <ivangalkin0720@gmail.com> 0.1.0.0
+- Initial RPM release
---
+++ solver_application/build/CMakeFiles/solver_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver_lib.dir/home/ivan/Work/lw6/lab06/solver_lib/solver.cpp.o"
+  "CMakeFiles/solver_lib.dir/home/ivan/Work/lw6/lab06/solver_lib/solver.cpp.o.d"
+  "libsolver_lib.a"
+  "libsolver_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
